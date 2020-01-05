86ea11f677b79d020e15bad3546128ac13b56e6e
bigya <bigyabajracharya@gmail.com>
2020-01-05 20:35:20 +0545

    3.2

--- index.js
+++ index.js
@@ -1,35 +1,45 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
 app.use(bodyParser.json());
 
-const persons = [{
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-    id: 2
-  },
-  {
-    'name': "Dan Abramov",
-    number: "12-43-234345",
-    id: 3
-  },
-  {
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-    id: 4
-  }];
-  
-app.get('/', (request, response)=>{
-    response.send('works!')
+const persons = [
+	{
+		name: "Ada Lovelace",
+		number: "39-44-5323523",
+		id: 2
+	},
+	{
+		name: "Dan Abramov",
+		number: "12-43-234345",
+		id: 3
+	},
+	{
+		name: "Mary Poppendieck",
+		number: "39-23-6423122",
+		id: 4
+	}
+];
+
+app.get("/", (request, response) => {
+	response.send("works!");
 });
 
-app.get('/api/persons', (request, response)=>{
-    response.json(persons);
+app.get("/api/persons", (request, response) => {
+	response.json(persons);
 });
 
-const PORT = 3002
+app.get("/info", (request, response) => {
+	const date = new Date();
+  const text = `Phonebook has info for ${persons.length} people as of:
+  ${date}`;
+   
+	response.send(text);
+});
+
+const PORT = 3002;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-})
+	console.log(`Server running on port ${PORT}`);
+});
