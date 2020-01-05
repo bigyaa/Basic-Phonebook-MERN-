0e4acd83354b14ebd8ad200891f485fb07333caa
Bigya Bajracharya <bigyabajracharya@gmail.com>
2020-01-05 14:31:36 +0545

    display data using express

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+
+const persons = [{
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523",
+    "id": 2
+  },
+  {
+    "name": "Dan Abramov",
+    "number": "12-43-234345",
+    "id": 3
+  },
+  {
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122",
+    "id": 4
+  }];
+  
+app.get('/', (request, response)=>{
+    response.send('works!')
+});
+
+app.get('/api/persons', (request, response)=>{
+    response.send(persons);
+});
+
+const PORT = 3002
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})
