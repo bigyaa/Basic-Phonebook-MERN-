b70ed82396c8ec56cbdbd864f6f9e6ec64b2a006
Bigya Bajracharya <bigyabajracharya@gmail.com>
2020-01-07 23:30:32 +0545

    3.16, 3.17, 3.18 - separate error handler middleware, implement put method in both front and backend, working api/persons/id and /info

--- index.js
+++ index.js
@@ -14,43 +14,51 @@ app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.static("build"));
 
+let peopleArray=[];
+
 app.get("/api/persons", (request, response) => {
   Person.find({}).then(people =>
-    response.json(people.map(person => person.toJSON()))
-  );
+    {
+      peopleArray=people;
+      response.json(people.map(person =>    
+       person.toJSON()))
+       } );
 });
 
-// app.get("/info", (request, response) => {
-//   const date = new Date();
-//   let numOfEntries;
+app.get("/info", (request, response) => {
+  const date = new Date();
+con
+  const text = `Phonebook has info for ${peopleArray.length} people as of:
+  ${date}`;
 
-//   Person.find({}).then(people => {
-//     console.log("iiiiiiiiiiiiiii", people);
-//     numOfEntries = people.length;
-//   });
+  response.send(text);
+});
 
-//   const text = `Phonebook has info for ${numOfEntries} people as of:
-//   ${date}`;
-
-//   response.send(text);
-// });
-
-app.get("/api/persons/:id", (request, response) => {
+app.get("/api/persons/:id", (request, response, next) => {
   const id = request.params.id;
 
   Person.findById(id)
     .then(person => response.json(person.toJSON()))
-    .catch(error => {
-      console.log(error);
-      response.status(404).end({ error: "Id doesn't exist" });
-    });
+    .catch(error => next(error));
 });
 
-app.delete("/api/persons/:id", (request, response) => {
+app.delete("/api/persons/:id", (request, response, next) => {
   const id = request.params.id;
 
   Person.findByIdAndRemove(id)
     .then(result => response.status(204).end())
+    .catch(error => next(error));
+});
+
+app.put("/api/persons/:id", (request, response, next) => {
+  const id = request.params.id;
+  const updatedPerson={
+    name: request.body.name,
+    number: request.body.number
+  };
+
+  Person.findByIdAndUpdate(id, updatedPerson, {new:true})
+    .then(result => response.json(result.toJSON()))
     .catch(error => next(error));
 });
 
@@ -63,6 +71,27 @@ app.post("/api/persons", (request, response) => {
     ? response.status(404).send({ error: "The name or number is missing" })
     : newPerson.save().then(savedPerson => response.json(savedPerson.toJSON()));
 });
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError' && error.kind === 'ObjectId') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else {
+    return response.status(404).send({ error: 'id doesnt exist.' })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => {
